22973d0b1114d06000af6979e52608d9d9f38a8e
jihun.beak <115356113+jihun1362@users.noreply.github.com>
2022-11-24 21:05:46 +0900

    Update Num1.java

--- 항해_알고리즘/Num1.java
+++ 항해_알고리즘/Num1.java
@@ -25,7 +25,7 @@ class Solution1 {
 }
 //2016년 1월 1일은 금요일입니다.2016년 a월 b일은 무슨 요일일까요?
 //2016년은 윤년입니다.
-public class Num1_2016년 {
+public class Num1 {
     public static void main(String[] args) {
         int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
